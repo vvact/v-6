38c83e66da382161bc0a053a08dd1b2227586e5b
Victor midega <vbm7752@gmail.com>
2024-01-08 14:29:22 +0300

    new

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,render_template,jsonify,request
+from database import load_jobs_from_db
+from database import load_job_from_db
+from database import add_application_to_db
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def home():
+    jobs = load_jobs_from_db()
+    return render_template('home.html', jobs= jobs,company_name = 'Local Jobs')
+
+@app.route('/api/jobs')
+def list_jobs():
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
+
+
+@app.route('/job/<id>')
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template('jobpage.html', job = job )
+
+@app.route('/job/<id>/apply',methods = ['post'])
+def apply_to_job(id):
+    data = request.form
+    job = load_job_from_db(id)
+    add_application_to_db(id, data)
+    return render_template('application_submitted.html',application = data, job = job)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine, text
+
+db_connection = 'mysql+pymysql://root:Victor123$@localhost/vitdb'
+engine = create_engine(db_connection)
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(row)
+        return jobs
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT * FROM jobs WHERE id = :val'), {'val': id})
+        row = result.first()
+
+        if row:
+            column_names = result.keys()
+            job_dict = {column: getattr(row, column) for column in column_names}
+            return job_dict
+        else:
+            return None
+
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+        
+        parameters = {
+            'job_id': job_id,
+            'full_name': data['full_name'],
+            'email': data['email'],
+            'linkedin_url': data['linkedin_url'],
+            'education': data['education'],
+            'work_experience': data['work_experience'],
+            'resume_url': data['resume_url'],
+        }
+
+        try:
+         conn.execute(query, parameters)
+         conn.commit()  # Make sure to commit the changes
+        except Exception as e:
+         print(f"Error executing query: {e}")
